9bb1b2d389974b4d1de4de415cc8ab6020234ad3
Milena Daflon <milenadaflon@gmail.com>
2025-11-29 19:34:02 -0300

    commit29/11

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Counter from "../Counter";
+
 
 function Header() {
     return (
         <header className={styles.header}>
             <span>No one sleeps in Tokyo</span>
+            <br></br>
+            <Counter />
 
             <nav>
               <Link to="/">Home</Link>
               <Link to="/watch">Watch</Link>
             </nav>
+
+            
         
         </header>
     )
